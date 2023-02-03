a3b3362cd24e682d4546097b4ea70acfe1b01102
niknikolaou <nik.nikolaou@gmail.com>
2023-02-03 19:44:00 +0200

    Check if user have 1155 for the game

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,16 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract DiamondScores 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface ERC1155 {
+    function balanceOf(address owner, uint256 id) external view returns (uint);
+}
+
+contract DiamondScores is Ownable 
 {
     mapping(address => string) private addressToNicknames;
     mapping(string => address) private nicknameToAddress;
     mapping(address => string) private scores;
+
     address[] private usersAddress;
 
+    address public wolf2dContractAddress;
 
+    
     event ScoreAdded(address player, string encryptedScore, string nickname);
     event NicknameAdded(address player, string nickname);
+
 
     function addNickname(string memory _nickname) public 
     {
@@ -40,6 +50,14 @@ contract DiamondScores
         return allScores;
     }
 
+         function getAllUsernames() public view returns (string[] memory) {
+        string[] memory allusernames = new string[](usersAddress.length);
+        for (uint i = 0; i < usersAddress.length; i++) {
+            allusernames[i] = addressToNicknames[usersAddress[i]];
+        }
+        return allusernames;
+    }
+
     function getAllUsers() public view returns (address[] memory) {
         return usersAddress;
     }
@@ -50,10 +68,26 @@ contract DiamondScores
 
         function getAddressByNickname(string memory nickname) public view returns (address) {
     return nicknameToAddress[nickname];
-}
+    }
 
 
     function getScore(address _player) public view returns (string memory) {
         return scores[_player];
+    }
+
+    function hasNFT( uint256 _tokenId, address user) public view returns (bool) {
+    // Get a reference to the NFT contract
+    ERC1155 nftContract = ERC1155(wolf2dContractAddress);
+
+    // Get the balance of the NFT for the user
+    uint balance = nftContract.balanceOf(user, _tokenId);
+
+    // Return true if the balance is greater than zero, false otherwise
+    return balance > 0;
+    }
+
+    //Administrator Functions
+    function setnftContractAddress(address address2dWolf) public onlyOwner {
+        wolf2dContractAddress = address2dWolf;
     }
 }
